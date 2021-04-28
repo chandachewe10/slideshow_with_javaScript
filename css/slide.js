791b2f5599f3e8d30c091476a20d584885566380
chanda chewe <chewec03@gmail.com>
2021-04-28 19:45:19 -0400

    simple slideshow with javascript

---
+++ css/slide.js
@@ -0,0 +1,18 @@
+function start(){        
+    document.getElementById('par').innerHTML= document.getElementById('restart').innerHTML;
+}
+
+function next(){        
+    document.getElementById('par').innerHTML= document.getElementById('pear').innerHTML;
+}
+function nextagain(){
+    document.getElementById('par').innerHTML=  document.getElementById('resume').innerHTML; 
+}
+
+
+function back(){        
+    document.getElementById('par').innerHTML=  document.getElementById('restart').innerHTML; 
+}
+function backagain(){
+    document.getElementById('par').innerHTML=  document.getElementById('pear').innerHTML; 
+}
